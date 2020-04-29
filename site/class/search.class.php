8edbdf718371f7e51b1170215a61f137c5a48332
auzou <auzou@users.noreply.github.com>
2020-04-29 15:04:20 +0200

    update

---
+++ site/class/search.class.php
@@ -0,0 +1,45 @@
+<?php 
+
+class Search
+{
+    private $searchList;
+    private $searchName;
+    
+    public function __construct()
+    {
+        $this->searchList = $this->getSearchList();
+        $this->searchName = (isset($_GET['search'])) ? htmlentities($_GET['search']) : NULL; 
+        $this->rewrite();        
+    }
+    private function getSearchList()
+    {
+        // name =>  uri
+        return [
+            'documentation' => 'documentation',
+            'home' => 'home',
+            'download' => 'download',
+            'sign-up' => 'sign-up',
+            'sign-in' => 'sign-in',
+            'account' => 'account',
+            'faq' => 'faq',
+            'schema' => 'schema',
+        ];
+    }
+    public function rewrite()
+    {
+        foreach($this->searchList as $key => $value)
+        {
+            if($key == $this->searchName)
+            {
+                header("Location: $value");
+            }
+        }
+        header('Location: home');
+        // failling
+    }
+}
+
+$search = new Search();
+
+
+?>
